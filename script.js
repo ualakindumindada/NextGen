1b3812c0a6ba9a8d8e0fcc97ee860a9851553d8b
U.A.Lakindu Mindada <ualakindumindadaofficial@gmail.com>
2026-01-13 16:49:06 +0530

    Update overall code.
    Add Logo image
    Add hamburger animation
    Add js code for hamburger

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Hamburger menu
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
 });
