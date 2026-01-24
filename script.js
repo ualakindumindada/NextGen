c8262bccb8f38d556b10a18c95aa120c3f419e72
U.A.Lakindu Mindada <ualakindumindadaofficial@gmail.com>
2026-01-24 17:48:59 +0530

    FAQ section done
    FAQ section done

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
   hamburger.classList.toggle("active");
 });
+
+document.querySelectorAll(".faq-question").forEach((question) => {
+  question.addEventListener("click", () => {
+    const faqItem = question.parentElement;
+    faqItem.classList.toggle("active");
+  });
+});
