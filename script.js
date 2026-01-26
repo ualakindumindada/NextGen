453963666b1457d73d440e0c739d70455d9d3648
U.A.Lakindu Mindada <ualakindumindadaofficial@gmail.com>
2026-01-26 23:10:15 +0530

    Add index.html animation
    I add some animation for section and some animation for counter section

--- script.js
+++ script.js
@@ -24,3 +24,63 @@ navLinks.forEach((link) => {
     link.classList.add("active");
   }
 });
+
+
+
+
+
+
+// Reveal Elements on Scroll
+
+const revealElements = document.querySelectorAll(".reveal");
+
+const revealObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+revealElements.forEach((el) => revealObserver.observe(el));
+
+// Counter Animation
+const counters = document.querySelectorAll(".counter");
+
+const counterObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const counter = entry.target;
+        const target = Number(counter.dataset.target);
+        const suffix = counter.dataset.suffix || "";
+
+        let current = 0;
+        const duration = 2000;
+        const steps = Math.ceil(duration / 16);
+        const stepValue = target / steps;
+
+        const updateCounter = () => {
+          current += stepValue;
+
+          if (current < target) {
+            counter.textContent = Math.round(current) + suffix;
+            requestAnimationFrame(updateCounter);
+          } else {
+            counter.textContent = target + suffix;
+          }
+        };
+
+        updateCounter();
+        observer.unobserve(counter);
+      }
+    });
+  },
+  { threshold: 0.3 },
+);
+
+counters.forEach((counter) => counterObserver.observe(counter));
